685bd448dc1d6a8f9b250e0a04622034225e89bb
Fralax <frayssinetr@gmail.com>
2016-10-15 00:24:45 +0200

    modele

---
+++ Modeles/modele.php
@@ -0,0 +1,25 @@
+<?php
+
+abstract class modele {
+
+  private $bdd;
+
+  protected function executerRequete($sql, $parametres = null) {
+    if ($parametres == null){
+      $resultat = $this->getBdd()->query($sql);
+    }
+    else {
+      $resultat = $this->getBdd()->prepare($sql);
+      $resultat->execute($parametres);
+    }
+    return $resultat;
+  }
+
+  private function getBdd() {
+    if ($this->bdd == null) {
+      $this->bdd = new PDO("mysql:host=localhost; dbname=teamhubp_teamhub; charset=utf8", 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    return $this->bdd;
+  }
+}
+?>
