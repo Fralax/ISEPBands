d20e91ea97ec6bc433aada0bee139205e4287d4c
Romain Frayssinet <romainfrayssinet@macbook-pro-de-romain.home>
2016-10-14 23:52:35 +0200

    recup

--- Vues/vueInscription.php
+++ Vues/vueInscription.php
@@ -1,13 +1,5 @@
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Isep band Inscription</title>
-  </head>
-  <body>
-    <form action="Controleurs/controleurInscription.php" method="post">
-      Email :
-      <input type="text" name="email">
-      <input type="submit" value="Valider">
-    </form>
-  </body>
-</html>
+<form action="" method="post">
+  Email :
+  <input type="text" name="email">
+  <input type="submit" value="Valider">
+</form>
