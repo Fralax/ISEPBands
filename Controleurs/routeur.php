bd7eb05d4c9d4b1f16bdcaa5d9cfab79b7d2a62d
Romain Frayssinet <romainfrayssinet@macbook-pro-de-romain.home>
2016-10-17 19:54:08 +0200

    inscription + connexion

--- Controleurs/routeur.php
+++ Controleurs/routeur.php
@@ -3,23 +3,26 @@
   require_once 'Controleurs/controleurAccueil.php';
   require_once 'Controleurs/controleurAnnuaire.php';
   require_once 'Controleurs/controleurInscription.php';
+  require_once 'Controleurs/controleurConnexion.php';
 
   class routeur{
 
     private $controleurAccueil;
     private $controleurAnnuaire;
     private $controleurInscription;
+    private $controleurConnexion;
 
     public function __construct(){
       $this->controleurAccueil = new controleurAccueil();
       $this->controleurAnnuaire = new controleurAnnuaire();
       $this->controleurInscription = new controleurInscription();
-      //session_start();
+      $this->controleurconnexion = new controleurConnexion();
+      session_start();
     }
 
     public function routerRequete(){
       $routeur = new routeur();
-      error_reporting(0);
+      //error_reporting(0);
       switch($_GET['page']){
 
         case 'accueil':
--- Vues/vueAccueil.php
+++ Vues/vueAccueil.php
@@ -1,3 +1,19 @@
 <?php $this->titre = "Accueil" ?>
 
-<a href="index.php?page=inscription"><p>Inscris-toi</p></a>
+<?php
+  if (isset($_SESSION['email'])) {
+    $a = 0;
+  } else{
+    $a = 1;
+  }
+?>
+
+<?php if ($a == 0): ?>
+  Vos informations :
+<?php endif; ?>
+
+<?php if ($a == 1): ?>
+  <div class="bienvenue">
+    <h1>Bienvenue sur le site d'ISEP Bands !</h1>
+  </div>
+<?php endif; ?>
