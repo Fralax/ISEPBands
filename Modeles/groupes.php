0fe60e22330d9734ec4fa3be038cc66c02337167
Fralax <frayssinetr@gmail.com>
2016-10-27 16:09:56 +0200

    modif viteuf de trucs nuls

---
+++ Modeles/groupes.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "Modeles/modele.php";
+
+class groupe extends modele {
+
+  public function creerGroupe($id, $nomGroupe){
+    $sql = 'INSERT INTO groupe (g_nom, g_createur) VALUES (:nomGroupe, :id)';
+    $creerGroupe = $this->executerRequete($sql, array('nomGroupe' => $nomGroupe, 'id' => $id));
+
+    $sql = 'INSERT INTO appartient (a_groupe, a_membre) VALUES (:nomGroupe, :id)';
+    $creerGroupe = $this->executerRequete($sql, array('nomGroupe' => $nomGroupe, 'id' => $id));
+  }
+
+}
+
+?>
