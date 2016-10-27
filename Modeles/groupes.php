776a93aa42cfa49c333bab311857845055cfaade
Camille Marchetti <Camille Marchetti>
2016-10-27 15:57:50 +0200

    avec l'ajout à la table appartient c'est mieux

--- Modeles/groupes.php
+++ Modeles/groupes.php
@@ -4,10 +4,12 @@ require_once "Modeles/modele.php";
 
 class groupe extends modele {
 
-  public function creerGroupe($id, $nomGroupe){ 
+  public function creerGroupe($id, $nomGroupe){
     $sql = 'INSERT INTO groupe (g_nom, g_createur) VALUES (:nomGroupe, :id)';
     $creerGroupe = $this->executerRequete($sql, array('nomGroupe' => $nomGroupe, 'id' => $id));
 
+    $sql = 'INSERT INTO appartient (a_groupe, a_membre) VALUES (:nomGroupe, :id)';
+    $creerGroupe = $this->executerRequete($sql, array('nomGroupe' => $nomGroupe, 'id' => $id));
   }
 
 }
