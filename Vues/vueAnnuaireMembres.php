347e80798c1b30b2bc38c54ba1a602117f551a29
Camille Marchetti <Camille Marchetti>
2017-01-05 19:27:22 +0100

    recup'

--- Vues/vueAnnuaireMembres.php
+++ Vues/vueAnnuaireMembres.php
@@ -3,7 +3,7 @@
   <div class="row row-centered membresAnnuaire">
     <?php foreach ($membres as list($id, $prenom, $nom, $photo)): ?>
       <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-centered membreAnnuaire">
-        <img class="photoAnnuaireMembre" src="<?php echo $photo ?>" />
+        <a href="index.php?page=profil&amp;id=<?php echo $id ?>"><img class="photoAnnuaireMembre" src="<?php echo $photo ?>" /></a>
         <a href="index.php?page=profil&amp;id=<?php echo $id ?>"> <?php echo $prenom." ".$nom ?></a>
       </div>
     <?php endforeach; ?>
